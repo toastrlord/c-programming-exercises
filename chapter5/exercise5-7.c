cf3879c91f0f2fe9b630f093bf2ae4b4f2314a71
Rob Morandini <robjmorandini@hotmail.com>
2021-08-05 20:46:15 -0400

    Add code for exercise 5-7 (to be modified)

---
+++ chapter5/exercise5-7.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+#define MAXLEN 1000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+main() {
+    int nlines;
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsort(lineptr, 0, nlines-1);
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("error: too many input lines\n");
+        return 1;
+    }
+}
+
+int getline(char *, int);
+char *alloc(int); 
+
+int readlines(char *lineptr[], int maxlines) {
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while((len = getline(line, MAXLEN)) > 0) {
+        if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+            return -1;
+        }
+        else {
+            line[len-1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    }
+    return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+    for (int i = 0; i < nlines; i++) {
+        printf("%s\n", lineptr[i]);
+    }
+}
