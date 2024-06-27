7e3e0050ec17c1c575243bb2684eac47fe5be085
Rob Morandini <rmorandini@mactaxcpa.com>
2024-06-26 21:42:30 -0400

    Refactor ex6, create ex7

--- Chapter 2/Exercise 6/ex6.c
+++ Chapter 2/Exercise 6/ex6.c
@@ -1,59 +1,25 @@
 #include <stdio.h>
 
+/*
+  Exercise 2-6:
+  Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits
+  unchanged
+*/
+
 int setbits(int x, int p, int n, int y);
 int sign(int x);
 void printBinary(int x, int minBits);
 
 main() {
-  int x = 4;
-  int y = 3;
-  int n = 3;
-  int p = 0; 
-  printBinary(x, 8);
-  printBinary(y, 8);
-  setbits(x, n, p, y);
-  //printf("%d\n", setbits(x, n, p, y));
+  int x = 23;
+  int y = 106;
+  printf("%d\n", setbits(x, 3, 3, y));
 }
 
 int setbits(int x, int p, int n, int y) {
-  int xMask = ~((~(~0 << n))) << (p + 1 - n);
-  //printf("xMask: ");
-  printBinary(xMask, 8);
-  int masked = x & xMask;
-  int yMask = (~(~0 << n) & y) << (p+ 1 - n);
+  int yMask = ~(~0 << n);
+  int xMask = ~((~(~0 << n)) << (p + 1 - n));
+  int xMasked = x & xMask;
+  int yMasked = (y & yMask) << (p + 1 - n);
   return x ^ y;
-}
-
-void printBinary(int x, int minBits) {
-  int sgn = sign(x);
-  int numBits, res;
-  res = x;
-  numBits = 0;
-  while ((x >> numBits) >= 0) {
-    numBits++;
-  }
-  numBits--;
-  printf("Num bits in %d: %d\n", x, numBits);
-  if (minBits > numBits) {
-    numBits = minBits;
-  }
-  for (int i = numBits; i > 0; i--) {
-    int bitIsOn = (x >> i) & ~0;
-    if (bitIsOn > 0) {
-      putchar('1');
-    } else {
-      putchar('0');
-    }
-  }
-  putchar('\n');
-}
-
-int sign(int x) {
-  if (x > 0) {
-    return 1;
-  }
-  if (x < 0) {
-    return -1;
-  }
-  return 0;
 }
---
+++ Chapter 2/Exercise 7/ex7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int invert(int x, int p, int n);
+
+main() {
+  printf("%d\n", invert(70, 4, 3));
+}
+
+int invert(int x, int p, int n) {
+  // get a mask such that values in p to p - n - 1 are 1's, 0's otherwise
+  // then XOR with x, this will invert any of the bits in those locations
+  int mask = ~(~0 << n) << (p - n + 1);
+  printf("Mask: %d\n", mask);
+  return x ^ mask;
+}
